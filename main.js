5ea2c1bafb3aa231a877d4d0c7d24c35a4b2aa5f
TEACHER <TEACHER@rivne.itstep.org>
2026-05-21 11:50:50 +0300

    final

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const APIKEY = "35fb68b7709beb3e39a474e03949e3cd"
 
+const currentCity = document.querySelector("#current-city")
 const mainTemp = document.querySelector("#main-temp")
+const mainFeelsLike = document.querySelector("#main-feels-like")
 const mainHumidity = document.querySelector("#humidity")
+const pressure = document.querySelector("#pressure")
 const windSpeed = document.querySelector("#wind-speed")
 const sunrise = document.querySelector("#sunrise")
 const sunset = document.querySelector("#sunset")
 const nearbyTemp = document.querySelector("#nearby-temp")
 const nearbyCity = document.querySelector("#nearby-city")
 const nearbyDescription = document.querySelector("#nearby-description")
+const nearbyTemp2 = document.querySelector("#nearby-temp2")
+const nearbyCity2 = document.querySelector("#nearby-city2")
+const nearbyDescription2 = document.querySelector("#nearby-description2")
 const searchInput = document.querySelector("#search-input")
 
 async function sendRequest() {
@@ -21,9 +27,13 @@ async function sendRequest() {
 
     console.log(data);
 
+    currentCity.innerHTML = city
+
     mainTemp.innerHTML = `${Math.round(data.main.temp)} °C`
+    mainFeelsLike.innerHTML = `feels like ${Math.round(data.main.feels_like)} °C`
     mainHumidity.innerHTML = `${data.main.humidity} %`
     windSpeed.innerHTML = `${data.wind.speed} km/h`
+    pressure.innerHTML = `${data.main.pressure} hPa`
 
     let sunriseUnix = data.sys.sunrise
     let sunriseTime = new Date(sunriseUnix * 1000)
@@ -45,6 +55,18 @@ async function sendRequest() {
     nearbyTemp.innerHTML = `${Math.round(dataCoord.main.temp)} °C`
     nearbyCity.innerHTML = `${dataCoord.name}`
     nearbyDescription.innerHTML = `${dataCoord.weather[0].main}`
+
+    let apiCoord2 = `https://api.openweathermap.org/data/2.5/weather?lat=${data.coord.lat + 0.3}&lon=${data.coord.lon - 0.3}&appid=${APIKEY}&units=metric&lang=uk`;
+
+    const responseCoord2 = await fetch(apiCoord2)
+    const dataCoord2 = await responseCoord2.json()
+
+    console.log(dataCoord2);
+
+    nearbyTemp2.innerHTML = `${Math.round(dataCoord2.main.temp)} °C`
+    nearbyCity2.innerHTML = `${dataCoord2.name}`
+    nearbyDescription2.innerHTML = `${dataCoord2.weather[0].main}`
+
 
 
     changeBackgroundImage(data.weather[0].main)
@@ -83,10 +105,10 @@ async function fiveDaysForecast(city) {
 
     console.log(days);
 
+    fc.innerHTML = ""
     for (const key in days) {
         fc.innerHTML += `<div class="day active"><p>${key}</p><span>${Math.round(days[key].max)}°C</span></div>`
     }
-    
 }
 
 
